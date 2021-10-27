fd2b5abc05c1eaa050555669ed4d024f62873654
Santiago Sánchez <icisan27@gmail.com>
2021-10-27 12:05:36 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import statistics
+n1 = int(input("Introduce el primer número: "))
+n2 = int(input("Introduce el segundo número: "))
+n3 = int(input("Introduce el tercer número: "))ç
+n = [n1,n2,n3]
