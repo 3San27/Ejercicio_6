9fc85fc22208000dc4b2047e44306b82c2979179
Santiago Sánchez <icisan27@gmail.com>
2021-10-27 14:34:32 +0200

    Cálculo de la media, mediana y varianza de tres números.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import statistics
 n1 = int(input("Introduce el primer número: "))
 n2 = int(input("Introduce el segundo número: "))
-n3 = int(input("Introduce el tercer número: "))ç
-n = [n1,n2,n3]
+n3 = int(input("Introduce el tercer número: "))
+n = {n1,n2,n3}
+media = statistics.mean(n)
+mediana = statistics.median(n)
+varianza = statistics.variance(n)
+print("La media es: ",media)
+print("La mediana es: ",mediana)
+print("La varianza es: ",varianza)
